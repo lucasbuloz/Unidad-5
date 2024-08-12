79c1e1a515278c291f2f31ab8684a0236bacce74
lucasbuloz <lucasbuloz@gmail.com>
2024-08-11 23:13:03 -0300

    ex

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def funcion3():
         if not id_sucursal:
             return redirect(url_for('funcion1'))
         
-        paquetes = Paquete.query.filter_by(entregado=0, idrepartidor=0, idsucursal=id_sucursal).all() #  filtro por sucursal
+        paquetes = Paquete.query.filter_by(entregado=0, idrepartidor=0).all() # , idsucursal=id_sucursal filtro por sucursal
         return render_template('funcion3.html', paquetes=paquetes, id_sucursal=id_sucursal)
 
 
